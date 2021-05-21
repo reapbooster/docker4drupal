cc0e7eae3b6bcfd65ed342d88f7709e66e227bd0
Jose Ramirez <webmaster@reapbooster.com>
2021-05-21 05:59:39 +0000

    Webpack: Change to dev, much faster :rocket:

--- codebase/webpack.config.js
+++ codebase/webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     index: './src/index.jsx',
     ReactDisplay: './web/themes/custom/custom_bootstrap_barrio/js/ReactDisplay.entry.jsx',
   },
-  mode: 'production',
+  mode: 'development',
   module: {
     rules: [
       {
@@ -13,6 +13,21 @@ module.exports = {
         loader: 'babel-loader',
         exclude: /(node_modules|bower_components)/,
       },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // JS to Style-Nodes
+          "style-loader",
+          // CSS => CommonJS
+          "css-loader", 
+          // SASS => CSS
+          "sass-loader",
+        ]
+      }
     ],
   },
   resolve: { extensions: ['*', '.js', '.jsx'] },
